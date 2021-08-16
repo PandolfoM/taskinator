eac3966a1dcbf18ad0f10452a9eb7b6d4214689d
Matthew Pandolfo <matt@pandolfo.com>
2021-08-16 09:33:13 -0400

    working delete button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var pageContentEl = document.querySelector('#page-content');
 var taskIdCounter = 0;
 
 var taskFormHandler = function(event) {
@@ -90,5 +91,19 @@ for (var i = 0; i < statusChoices.length; i++) {
 return actionContainerEl;
 };
 
+var taskButtonHandler = function(event) {
+  console.log(event.target);
 
+  if (event.target.matches(".delete-btn")) {
+    var taskId = event.target.getAttribute("data-task-id");
+    deleteTask(taskId);
+  }
+};
+
+var deleteTask = function(taskId) {
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+  taskSelected.remove();
+};
+
+pageContentEl.addEventListener("click", taskButtonHandler);
 formEl.addEventListener("submit", taskFormHandler);
